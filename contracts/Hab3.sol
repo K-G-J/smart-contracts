471069e45fa30901533508472200c8aed53dca71
"Kate" <"johnson.kat.g@gmail.com">
2022-04-19 17:37:48 -0400

    added events

--- contracts/Hab3.sol
+++ contracts/Hab3.sol
@@ -26,8 +26,14 @@ contract Hab3 {
     mapping(uint256 => Goal) private idToGoal;
     mapping (address => Goal[]) private userGoals;
 
+    event GoalAdded(address user, string goal, uint time);
+    event StepAdded(address user, string step, uint time);
+    event StepCompleted(address user, string step, uint time);
+    event GoalCompleted(address user, string goal, uint time);
+
     constructor() {
-        // mint tokens 
+        /* mint tokens */
+        /* create some different types of ERC155 tokens to reward users for completetion */
     }
 
     function setGoal (string calldata _goal, string[] calldata _steps) public payable {
@@ -40,5 +46,42 @@ contract Hab3 {
         idToGoal[newGoalId].goal = _goal;
         idToGoal[newGoalId].createdAt = block.timestamp;
         userGoals[msg.sender].push(idToGoal[newGoalId]);
+        emit GoalAdded(msg.sender, _goal, block.timestamp);
+    }
+
+    function addStep (uint _goalId, string calldata _step) public payable {
+        uint stepCount = idToGoal[_goalId].steps.length;
+        idToGoal[_goalId].steps.push(Step(stepCount+1, _step, false));
+        emit StepAdded(msg.sender, _step, block.timestamp);
+    }
+
+    function completeStep(uint _goalId, uint _stepId) public payable returns(Step[] memory) {
+        Step[] memory stepArr = idToGoal[_goalId].steps;
+        for (uint i = 0; i < stepArr.length; i++) {
+            if(stepArr[i].id == _stepId) {
+                stepArr[i].completed = true;
+                // some type of token reward 
+                emit StepCompleted(msg.sender, stepArr[i].step, block.timestamp);
+            }
+        }
+        return stepArr;
+    }
+
+    function completeGoal(uint _goalId) public payable {
+        idToGoal[_goalId].completed = true;
+        emit GoalCompleted(msg.sender, idToGoal[_goalId].goal, block.timestamp);
+        // some type of token reward 
+    }
+
+    function getUserGoals() public view returns (Goal[] memory) {
+
+    }
+
+    function getCurrentGoals() public view returns (Goal[] memory)  {
+
+    }
+
+    function getCompletedGoals() public view returns (Goal[] memory)  {
+
     }
 }
