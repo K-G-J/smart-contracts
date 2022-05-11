6817cad622ae2734f6dc5c4b15e63925de5c0d62
"Kate" <"johnson.kat.g@gmail.com">
2022-05-11 19:40:26 -0400

    added comments

--- contracts/SampleVault.sol
+++ contracts/SampleVault.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
-/* The idea for the vault is to accept funds and then rebalance back to a 60% ETH 40% DAI stablecoins */
-
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -22,7 +20,7 @@ interface IUniswapRouter is ISwapRouter {
 
 // Add deposit function for WETH
 interface DepositableERC20 is IERC20 {
-  function deposit() external payable;
+  function deposit() external payable; // deposit ETH to WETH to interact as ERC20
 }
 
 contract SampleVault {
@@ -32,12 +30,12 @@ contract SampleVault {
   address public wethAddress = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
   address public uinswapV3QuoterAddress = 0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6;
   address public uinswapV3RouterAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
-  address public chainLinkETHUSDAddress = 0x9326BFA02ADD2366b30bacB125260Af641031331;
+  address public chainLinkETHUSDAddress = 0x9326BFA02ADD2366b30bacB125260Af641031331; // oracle to get latest price
 
   uint public ethPrice = 0;
-  uint public usdTargetPercentage = 40;
+  uint public usdTargetPercentage = 40; // 60% in Ethereum and 40% in Dai
   uint public usdDividendPercentage = 25; // 25% of 40% = 10% Annual Drawdown
-  uint private dividendFrequency = 3 minutes; // change to 1 years for production
+  uint private dividendFrequency = 3 minutes; // change to years for production to setup prepetual fund 
   uint public nextDividendTS;
   address public owner;
 
@@ -58,28 +56,31 @@ contract SampleVault {
   }
 
   function getDaiBalance() public view returns(uint) {
-    return daiToken.balanceOf(address(this));
+    return daiToken.balanceOf(address(this)); // balance of Dai token
   }
 
   function getWethBalance() public view returns(uint) {
-    return wethToken.balanceOf(address(this));
+    return wethToken.balanceOf(address(this)); // balance of wrapped ETH
   }
 
+  // returns total balance of vault
   function getTotalBalance() public view returns(uint) {
-    require(ethPrice > 0, 'ETH price has not been set');
+    require(ethPrice > 0, "ETH price has not been set");
     uint daiBalance = getDaiBalance();
     uint wethBalance = getWethBalance();
-    uint wethUSD = wethBalance * ethPrice; // assumes both assets have 18 decimals
+    uint wethUSD = wethBalance * ethPrice; // US dollar value of WETH (assumes both assets have 18 decimals)
     uint totalBalance = wethUSD + daiBalance;
     return totalBalance;
   }
 
+  // get quote for transaction from Uniswap
   function updateEthPriceUniswap() public returns(uint) {
     uint ethPriceRaw = quoter.quoteExactOutputSingle(daiAddress,wethAddress,3000,100000,0);
     ethPrice = ethPriceRaw / 100000;
     return ethPrice;
   }
 
+  // get different quote from ChainLink
   function updateEthPriceChainlink() public returns(uint) {
     int256 chainLinkEthPrice = EACAggregatorProxy(chainLinkETHUSDAddress).latestAnswer();
     ethPrice = uint(chainLinkEthPrice / 100000000);
@@ -91,10 +92,10 @@ contract SampleVault {
     uint24 fee = 3000;
     address recipient = address(this);
     uint256 amountIn = amountUSD; // includes 18 decimals
-    uint256 amountOutMinimum = 0;
+    uint256 amountOutMinimum = 0; // change to avoid front running on mainnet 
     uint160 sqrtPriceLimitX96 = 0;
     emit myVaultLog('amountIn', amountIn);
-    require(daiToken.approve(address(uinswapV3RouterAddress), amountIn), 'DAI approve failed');
+    require(daiToken.approve(address(uinswapV3RouterAddress), amountIn), "DAI approve failed"); // approve Uniswap to spend for ERC20 token 
     ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams(
       daiAddress,
       wethAddress,
@@ -106,7 +107,7 @@ contract SampleVault {
       sqrtPriceLimitX96
     );
     uniswapRouter.exactInputSingle(params);
-    uniswapRouter.refundETH();
+    uniswapRouter.refundETH(); // return any ETH to account 
   }
 
   function sellWeth(uint amountUSD) internal {
@@ -126,39 +127,41 @@ contract SampleVault {
       amountOut,
       amountInMaximum,
       sqrtPriceLimitX96
-    );
+    ); // declare amount out in US dollar
     uniswapRouter.exactOutputSingle(params);
     uniswapRouter.refundETH();
   }
 
+  // takes portfolio percentage to rebalance back to 60-40 strategy 
   function rebalance() public {
     require(msg.sender == owner, "Only the owner can rebalance their account");
     uint usdBalance = getDaiBalance();
-    uint totalBalance = getTotalBalance();
-    uint usdBalancePercentage = 100 * usdBalance / totalBalance;
-    emit myVaultLog('usdBalancePercentage', usdBalancePercentage);
+    uint totalBalance = getTotalBalance(); 
+    uint usdBalancePercentage = 100 * usdBalance / totalBalance; // working with integers so * 100 first
+    emit myVaultLog("usdBalancePercentage", usdBalancePercentage);
     if (usdBalancePercentage < usdTargetPercentage) {
       uint amountToSell = totalBalance / 100 * (usdTargetPercentage - usdBalancePercentage);
-      emit myVaultLog('amountToSell', amountToSell);
+      emit myVaultLog("amountToSell", amountToSell);
       require (amountToSell > 0, "Nothing to sell");
-      sellWeth(amountToSell);
+      sellWeth(amountToSell); // case: sell WETH to balance 
     } else {
       uint amountToBuy = totalBalance / 100 * (usdBalancePercentage - usdTargetPercentage);
-      emit myVaultLog('amountToBuy', amountToBuy);
+      emit myVaultLog("amountToBuy", amountToBuy);
       require (amountToBuy > 0, "Nothing to buy");
-      buyWeth(amountToBuy);
+      buyWeth(amountToBuy); // case: buy WETH to balance
     }
   }
 
   function annualDividend() public {
     require(msg.sender == owner, "Only the owner can drawdown their account");
-    require(block.timestamp > nextDividendTS, 'Dividend is not yet due');
+    require(block.timestamp > nextDividendTS, "Dividend is not yet due");
     uint balance = getDaiBalance();
     uint amount = (balance * usdDividendPercentage) / 100;
-    nextDividendTS = block.timestamp + dividendFrequency;
-    daiToken.safeTransfer(owner, amount);
+    nextDividendTS = block.timestamp + dividendFrequency; // set timestamps before external call
+    daiToken.safeTransfer(owner, amount); // use safeTransfer to transfer owner due amount this dividend
   }
 
+  // remove entire Dai and WETH balance from account (if not using dividend)
   function closeAccount() public {
     require(msg.sender == owner, "Only the owner can close their account");
     uint daiBalance = getDaiBalance();
@@ -171,16 +174,18 @@ contract SampleVault {
     }
   }
 
+  // default function that allows contract to recieve ETH
   receive() external payable {
     // accept ETH, do nothing as it would break the gas fee for a transaction
   }
 
+  // Wrap ETH after ETH is recieved by the contract
   function wrapETH() public {
     require(msg.sender == owner, "Only the owner can convert ETH to WETH");
     uint ethBalance = address(this).balance;
     require(ethBalance > 0, "No ETH available to wrap");
     emit myVaultLog('wrapETH', ethBalance);
-    wethToken.deposit{ value: ethBalance }();
+    wethToken.deposit{ value: ethBalance }(); // Wrap ETH to use in portfolio using DepositableERC20 interface
   }
   
 }
