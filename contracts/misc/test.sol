0dc2f64fb4bc1b574ade93f9a6ee4ceb1e4633ba
"Kate" <"johnson.kat.g@gmail.com">
2022-06-02 20:42:10 -0400

    updated charity raffle

--- contracts/misc/test.sol
+++ contracts/misc/test.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.12;
 
 error FundingContractFailed();
 
 contract test {
-    constructor (uint256 _amount) {
-        (bool success, ) = payable(address(this)).call{value: _amount}("");
-        if (!success) {
-            revert FundingContractFailed();
-            }
-    } 
+    
+    function concat(string memory one, string memory two) external pure returns (string memory concatedString) {
+        concatedString = string.concat(one, " ", two);
+    }
 
 }
