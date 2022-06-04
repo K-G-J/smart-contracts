05de02e6ab68a588f2347e0a7831d3f231135c49
"Kate" <"johnson.kat.g@gmail.com">
2022-06-03 23:40:33 -0400

    updated charity raffle

--- contracts/misc/CharityRaffle.sol
+++ contracts/misc/CharityRaffle.sol
@@ -216,26 +216,25 @@ contract CharityRaffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         donations[i_charity3] = 0;
         if (tie) {
             handleTie(randomWords, charity1Total, charity2Total, charity3Total);
-        } else {
-            // not a tie
-            if (charity1Total > charity2Total && charity1Total > charity3Total) {
-                // charity1 wins
-                s_highestDonations = charity1Total;
-                s_charityWinner = i_charity1;
-                emit CharityWinnerPicked(i_charity1);
-            }
-            if (charity2Total > charity1Total && charity2Total > charity3Total) {
-                // charity2 wins
-                s_highestDonations = charity2Total;
-                s_charityWinner = i_charity2;
-                emit CharityWinnerPicked(i_charity2);
-            }
-            if (charity3Total > charity1Total && charity3Total > charity2Total) {
-                // charity3 wins
-                s_highestDonations = charity3Total;
-                s_charityWinner = i_charity3;
-                emit CharityWinnerPicked(i_charity3);
-            }
+        }
+        // not a tie
+        if (charity1Total > charity2Total && charity1Total > charity3Total) {
+            // charity1 wins
+            s_highestDonations = charity1Total;
+            s_charityWinner = i_charity1;
+            emit CharityWinnerPicked(i_charity1);
+        }
+        if (charity2Total > charity1Total && charity2Total > charity3Total) {
+            // charity2 wins
+            s_highestDonations = charity2Total;
+            s_charityWinner = i_charity2;
+            emit CharityWinnerPicked(i_charity2);
+        }
+        if (charity3Total > charity1Total && charity3Total > charity2Total) {
+            // charity3 wins
+            s_highestDonations = charity3Total;
+            s_charityWinner = i_charity3;
+            emit CharityWinnerPicked(i_charity3);
         }
         emit WinnerPicked(recentWinner);
     }
@@ -430,6 +429,10 @@ contract CharityRaffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         charities[1] = i_charity2;
         charities[2] = i_charity3;
         return charities;
+    }
+
+    function getDonations(address charity) external view returns (uint256) {
+        return donations[charity];
     }
 
     function getEntranceFee() external view returns (uint256) {
